af9d92afa3dea947aa9933a119db1e368b7ba6fc
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-02-26 19:24:42 -0500

    super legit now with a little style

--- team-builder/src/components/AvengerForm.js
+++ team-builder/src/components/AvengerForm.js
@@ -19,31 +19,33 @@ const AvengerForm = props => {
   const submitForm = event => {
       event.preventDefault();
       props.addNewAvenger(avenger)
+      setAvenger({alias: '',identity: '', superpower: '', home:'', avatar:''})
   }
 
   return(
+    <div className="form-container">
+        <form onSubmit={submitForm} >
+            <label htmlFor="alias">Avenger:</label>
+            <input onChange={handleChanges} id="alias" name="alias" type="text" value={avenger.alias}/>
 
-    <form onSubmit={submitForm} >
-        <label htmlFor="alias">Avenger:</label>
-        <input onChange={handleChanges} id="alias" name="alias" type="text" value={avenger.alias}/>
+            <label htmlFor="identity">Real Name:</label>
+            <input onChange={handleChanges} id="identity" name="identity" type="text" value={avenger.identity}/>
 
-        <label htmlFor="identity">Real Name:</label>
-        <input onChange={handleChanges} id="identity" name="identity" type="text" value={avenger.identity}/>
+            <label htmlFor="superpower">Power(s):</label>
+            <input onChange={handleChanges} id ="superpower" name="superpower" type="text" value={avenger.superpower}></input> 
 
-        <label htmlFor="superpower">Power(s):</label>
-        <input onChange={handleChanges} id ="superpower" name="superpower" type="text" value={avenger.superpower}></input> 
+            <label htmlFor="home">Homeworld:</label>
+            <input onChange={handleChanges} id ="home" name="home" type="text" value={avenger.home}></input>
 
-        <label htmlFor="home">Homeworld:</label>
-        <input onChange={handleChanges} id ="home" name="home" type="text" value={avenger.home}></input>
+            <label htmlFor="avatar">Image:</label>
+            <input onChange={handleChanges} id ="avatar" type="url" name="avatar" placeholder="Enter url" value={avenger.avatar}></input> 
 
-        <label htmlFor="avatar">Image:</label>
-        <input onChange={handleChanges} id ="avatar" type="url" name="avatar" placeholder="Enter url" value={avenger.avatar}></input> 
+            {/* <label htmlFor="avatar">Image:</label>
+            <input onChange={handleChanges} id ="avatar" type="file" name="avatar" accept="image/png, image/jpeg"></input>      */}
 
-        {/* <label htmlFor="avatar">Image:</label>
-        <input onChange={handleChanges} id ="avatar" type="file" name="avatar" accept="image/png, image/jpeg"></input>      */}
-
-        <button type='submit'>Add Avenger</button>
-    </form>
+            <button type='submit'>Add Avenger</button>
+        </form>
+     </div>
   );
 };
 
--- team-builder/src/components/Avengers.js
+++ team-builder/src/components/Avengers.js
@@ -11,9 +11,9 @@ const Avengers  = props => {
                 <img src={avenger.avatar} alt="avenger-avatar"></img>
             </div>
             <h2>{avenger.alias}</h2>
-            <p>{avenger.identity}</p>
-            <p>{avenger.superpower}</p>
-            <p>{avenger.home}</p>
+            <p>Real Name: {avenger.identity}</p>
+            <p>Superpower: {avenger.superpower}</p>
+            <p>Homeworld: {avenger.home}</p>
             </div>
         ))}
         </div>
